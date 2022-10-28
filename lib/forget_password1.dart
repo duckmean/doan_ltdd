270489208ea7cc9e2d67ff2faef6d052be14e19c
duckmean <0306201155@caothang.edu.vn>
2022-10-28 10:19:25 +0700

    giaodien

---
+++ lib/forget_password1.dart
@@ -0,0 +1,172 @@
+import 'package:doan_ltdd/forget_password2.dart';
+import 'package:doan_ltdd/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class Forget_password1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+            maxWidth: MediaQuery.of(context).size.width,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 111, 190, 255),
+              Color.fromARGB(255, 111, 190, 255),
+            ], begin: Alignment.topLeft, end: Alignment.centerRight),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 36.0, horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Quên Mật Khẩu",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                                flex: 4,
+                                child: Container(
+                                  padding: const EdgeInsets.all(15),
+                                  child: const TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'Nhập Email',
+                                      labelText: 'Email',
+                                      border: OutlineInputBorder(),
+                                      prefixIcon: Icon(Icons.email),
+                                    ),
+                                  ),
+                                )),
+                            Expanded(
+                              flex: 1,
+                              child: TextButton(
+                                child: const Text('Gửi'),
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.blue),
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Container(
+                      //   padding: const EdgeInsets.all(15),
+                      //   child: const TextField(
+                      //     decoration: InputDecoration(
+                      //       hintText: 'Nhập Email',
+                      //       labelText: 'Email',
+                      //       border: OutlineInputBorder(),
+                      //       prefixIcon: Icon(Icons.email),
+                      //     ),
+                      //   ),
+                      // ),
+
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Nhập mã xác nhận Email',
+                            labelText: 'Nhập mã xác nhận Email',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.check),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          TextButton(
+                            child: const Text(
+                              'Quay lại',
+                              style: TextStyle(fontSize: 17),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginScreen(),
+                                  ));
+                            },
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Forget_password2(),
+                                ));
+                          }, //bo sung 2
+                          child: const Text('Xác nhận',
+                              style: TextStyle(fontSize: 20)),
+                          style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.all(Colors.red),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.all(15)),
+                            fixedSize:
+                                MaterialStateProperty.all(const Size(150, 60)),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,182 @@
+import 'package:doan_ltdd/forget_password1.dart';
+import 'package:doan_ltdd/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+            maxWidth: MediaQuery.of(context).size.width,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 111, 190, 255),
+              Color.fromARGB(255, 111, 190, 255),
+            ], begin: Alignment.topLeft, end: Alignment.centerRight),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 36.0, horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Đăng Nhập",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Nhập tài khoản',
+                            labelText: 'Tài khoản',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        child: const TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Nhập mật khẩu',
+                            labelText: 'Mật khẩu',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.password),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          TextButton(
+                            child: const Text(
+                              'Quên mật khẩu',
+                              style: TextStyle(fontSize: 17),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Forget_password1(),
+                                  ));
+                            },
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        child: ElevatedButton(
+                          onPressed: () {}, //bo sung 2
+                          child: const Text('Đăng Nhập',
+                              style: TextStyle(fontSize: 20)),
+                          style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.all(Colors.red),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.all(15)),
+                            fixedSize:
+                                MaterialStateProperty.all(const Size(150, 60)),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text('Nếu bạn chưa có tài khoản?',
+                              style: TextStyle(fontSize: 16)),
+                          TextButton(
+                            child: const Text(
+                              'Đăng ký',
+                              style: TextStyle(fontSize: 17),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Register(),
+                                  ));
+                              //signup screen
+                            },
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'Hoặc đăng nhập bằng ',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      )
+                      // Container(
+                      //   child: Center(
+                      //   child: RichText(
+                      //     text: TextSpan(
+                      //       text: 'Nếu bạn chưa có tài khoản?',
+                      //       style: TextStyle(color: Colors.black, fontSize: 20),
+                      //        children: <TextSpan>[
+                      //         TextSpan(text: ' Đăng ký',
+                      //         style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+                      //          recognizer: TapGestureRecognizer()
+                      //          ..onTap = () {}
+                      //         ),
+                      //        ]
+                      //   ),
+                      //   ),
+                      // ),
+                      // ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
