48f7fe1e2af310667308591b82260858d329b674
duckmean <0306201155@caothang.edu.vn>
2022-12-09 20:50:56 +0700

    test api doi mat khau

--- lib/pages/login/login_screen.dart
+++ lib/pages/login/login_screen.dart
@@ -61,6 +61,23 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  // void login(String username, password) async {
+  //   try {
+  //     Response response = await post(Uri.parse('https://reqres.in/api/login'),
+  //         body: {'username': username, 'password': password});
+
+  //     if (response.statusCode == 200) {
+  //       var data = jsonDecode(response.body.toString());
+  //       print(data['token']);
+  //       print('Login successfully');
+  //     } else {
+  //       print('failed');
+  //     }
+  //   } catch (e) {
+  //     print(e.toString());
+  //   }
+  // }
+
   Future getDataLoginFromSharedPreferences() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
     pref.getString('login');
