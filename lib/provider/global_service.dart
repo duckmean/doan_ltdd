28367e08da1a63476afc9ca72c3e84f534819ff1
duckmean <0306201155@caothang.edu.vn>
2022-12-09 20:38:31 +0700

    update link real device

--- lib/provider/global_service.dart
+++ lib/provider/global_service.dart
@@ -2,7 +2,10 @@ import 'package:doan_ltdd/Appcolor/appcolor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//link dien thoai ao
 const String baseURL = "http://10.0.2.2:8000/api/";
+//link dien thoai thiet
+// const String baseURL = "http://192.168.1.100/api/";
 const Map<String, String> hearders = {"Content-Type": "application/json"};
 
 errorSnackbar(BuildContext context, String text) {
